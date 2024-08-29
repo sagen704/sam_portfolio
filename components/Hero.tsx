41c48e153e3be8072b2a91b3090b02c7a083f1a4
sagen704 <hagensa615@gmail.com>
2024-08-29 11:34:22 -0600

    Overflow Errors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,9 +4,9 @@ import Hero_Descriptions from "./Hero_Descriptions";
 
 const Hero = () => {
   return (
-    <div className="flex h-screen items-center">
+    <div className="flex h-screen">
       <div className="flex-1 flex flex-col pt-[30px] md:pt-[130px] sm:pt-[100px] md:pr-[40px] pr-[20px] pl-[75px] sm:pl-[125px] lg:pl-[250px]">
-        <h1 className=" text-xl md:text-4xl text-[#00AFEA] font-courier-new font-semibold">
+        <h1 className="pt-10 text-xl md:text-4xl text-[#00AFEA] font-courier-new font-semibold">
           Hello, I&apos;m
         </h1>
         <div className="py-[30px] text-white text-[40px] md:text-8xl font-bold ">
